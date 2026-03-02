89e6dbd8a857cc85c432755acb8c0cbaf2e3d9da
wmoralesdev <walterrafael26@gmail.com>
2026-03-01 19:18:01 -0600

    chore(config): update eslint, tsconfig, vite and gitignore
    Extend config for Convex, path aliases, and new app structure.

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'convex/_generated']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
